8c93c40cfcfff89e96f88274630a662c9fa1cb37
PeterHdd <peterhd3412@gmail.com>
2018-10-21 20:14:12 +0300

    initial commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,44 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  public tap:number=0;
+  public pressed:number=0;
+  public results:string;
 
+  constructor(public navCtrl: NavController) {
+    
+    this.results="Tap Twice, press four times"
+  
   }
 
-}
+  tapEvent(e){
+      this.tap++;
+  }
+
+  pressEvent(e){
+    this.pressed++;
+    
+  }
+
+  reset(){
+    this.tap=0;
+    this.pressed=0;
+  
+  }
+
+  didWin(){
+    return this.tap==2&&this.pressed==4;
+    }
+
+    resetPressed(){
+      this.pressed=0;
+    }
+
+    resetTap(){
+      this.tap=0;
+    }
+    
+  }
+
+
+
